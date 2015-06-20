21408e1637d240107fa069a7035ebed56d55fe1a
nomilous <richard@clue.co.za>
2015-06-20 22:03:47 +0200

    entrypoint

---
+++ test/in_test.js
@@ -0,0 +1,59 @@
+objective('In', function(should) {
+
+  // trace.filter = true;
+
+  it('pollutes the global namespace', {
+
+    why: "Extenders need only define global.$$in.<action>.<actor>" +
+         "to integrate their argument infuser."
+
+  }, function() {
+    should.exist($$in);
+  });
+
+
+  context('formatting', function(In, Format, Action) {
+
+    it('calls format.perform with each argument', function(done) {
+
+      // wait(Action); return
+
+      Action.stub(function perform(defer){
+        defer.resolve();
+      });
+      Format.does(
+        function perform(opts, accum, arg){
+          arg.name.should.equal('arg1');
+        },
+        function perform(opts, accum, arg){
+          arg.name.should.equal('arg2');
+        },
+        function perform(opts, accum, arg){
+          arg.name.should.equal('arg3');
+        }
+      );
+      In(function(arg1, arg2, arg3) {}).then(function() {
+        done();
+      });
+    });
+  });
+
+  
+  context('action', function(In, Format, Action) {
+
+    it('calls action.perform with each argument', function(done) {
+      Format.stub(function perform() {});
+      Action.does(
+        function perform(defer) { defer.resolve() },
+        function perform(defer) { defer.resolve() },
+        function perform(defer) { defer.resolve() }
+      )
+      In(function(arg1, arg2, arg3) {}).then(function() {
+        done();
+      });
+    })
+  });
+
+  
+
+});
