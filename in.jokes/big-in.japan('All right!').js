32860709d909cc04951a99b4a4c237c4b4bc93fe
nomilous <richard@clue.co.za>
2015-06-24 14:15:24 +0200

    big in japan

--- in.jokes/big-in.japan('All right!').js
+++ in.jokes/big-in.japan('All right!').js
@@ -6,22 +6,26 @@ $$in.expanders.japan = function(delay, possibly) { // in.as.possibly in a later 
                                                   // seems a bit excessive tho...
   $$in.promise(function(resolve) {
 
-    setTimeout(function Because_You_ll_Know( when ) { // in.as.you 're big in Japan!
+    setTimeout(function Because_You_ll_Know( when ) { // in.as.you.re {{big('in Japan!'}}
                                                      // 
                                                     // nope... this injection does
                                                    //          not happn (yet?)
                                                   //                     (impossible?)
                                                  //                      i('suspect so')
+                                                // 
+                                               // unless...  
         resolve([
           'Big in Japan-be-tight',
           'Big in Japan ooh the eastern sea\'s so blue',
           'Big in Japan-alright'
         ])
+
     }, delay);
   })
 }
 
-$$in(function( 
-    Big, // in. {{expand.japan(100)}}
-     In // in. {{console.log line for line in $p.big}}
+$$in(function(
+   Big, // in. {{expand.japan(100)}}
+    In // in. {{console.log line for line in Big}}
 ){/* Japan */});
+
