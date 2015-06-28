b29a9d6ed3c11a9eca85f5675c866fc961e5cdc0
nomilous <richard@clue.co.za>
2015-06-28 18:16:56 +0200

    in.cognito

---
+++ in.jokes/in.cognito.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+require('../');
+request = require('request');
+
+scope = {
+  get: $$in(function(resolve, reject){
+    
+  })
+}
+
+$$in(scope, function(
+  cognito, // in.as.js.function {{get()}}
+  resolve
+) {
+
+
+})
