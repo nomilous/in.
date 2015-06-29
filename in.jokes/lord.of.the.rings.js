0e390f1b64cfd04b10781b2944ffb4bec8a233e9
nomilous <richard@clue.co.za>
2015-06-29 12:47:54 +0200

    lord of the rings

--- in.jokes/lord.of.the.rings.js
+++ in.jokes/lord.of.the.rings.js
@@ -4,10 +4,10 @@ require('../');
 
 function TheOneRing() {
 
-  $$in(function           (     one) { // in.as.ring To.rule them {{resolve Infinity}}
-  }).then($$in(function   (all, one) { // in.as.ring To.bind them {{resolve Infinity}}
-  })).then($$in(function  (all, one) { // in.as.ring To.bring them {{throw Infinity}}
-  })).catch($$in(function (all, and) { // in. The.darkness find them!
+  $$in(function            (     one) { // in.as.ring To.rule them {{resolve Infinity}}
+  }).then($$in(function    (all, one) { // in.as.ring To.bind them {{resolve Infinity}}
+  })).then($$in(function   (all, one) { // in.as.ring To.bring them {{ throw Infinity }}
+  })) .catch($$in(function (all, and) { // in. The.darkness find them!
   }));
 
 }
